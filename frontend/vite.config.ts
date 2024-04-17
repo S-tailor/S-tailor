e96eb5f1f30c07f3a7a9fd8367cf751bb6252999
cyberjam <jamsince1994@naver.com>
2024-04-17 20:31:04 +0900

    S10P31B107-76-fe-chore-pwa-init

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        icons: [
+          {
+            src: 'icons/favicon-196x196.png',
+            type: 'image/png',
+            sizes: '196x196'
+          },
+          {
+            src: 'icons/favicon-196x196.png',
+            type: 'image/png',
+            sizes: '196x196',
+            purpose: 'maskable'
+          },
+          {
+            src: 'icons/favicon-512x512.png',
+            type: 'image/png',
+            sizes: '512x512'
+          },
+          {
+            src: 'icons/favicon-512x512.png',
+            type: 'image/png',
+            sizes: '512x512',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
+  ]
 })
