32d7ddc3ca0acaa8554ff28c7d2caa0d06dffd18
JoSangmin <139518200+JOSANGMIN777@users.noreply.github.com>
2024-04-26 12:27:05 +0900

    '2'

--- frontend/src/components/mobile/main/Main.tsx
+++ frontend/src/components/mobile/main/Main.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {useNavigate} from 'react-router-dom'
 
 const Main: React.FC = () => {
+  const navigate = useNavigate()
+  const moveToEmail = () => {
+    navigate('/mobile/login')
+  }
   return (
     <div>
+      <button onClick={moveToEmail}>시작하기</button>
       <h1>Main Component</h1>
     </div>
   )
--- frontend/src/pages/mobile/main/index.tsx
+++ frontend/src/pages/mobile/main/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet} from 'react-router-dom'
+
 
 const Main: React.FC = () => {
+ 
+
+
   return (
     <div>
       <Outlet />
