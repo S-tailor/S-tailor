505609664e31ec9adac1a9435dda65eec16eaeff
JIEUN LEE <jin3409975@gmail.com>
2024-04-27 01:36:04 +0900

    S10P31B107-43-fe-feature: 회원가입 버튼 생성 및 페이지 이동 구현

--- frontend/src/components/mobile/signup/Signup.tsx
+++ frontend/src/components/mobile/signup/Signup.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { startTransition } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { userCreate } from '@/api/apiUser'
 
 const Signup: React.FC = () => {
+  const navigate = useNavigate()
+
+  const SignupClick = () => {
+    startTransition(() => {
+      navigate('/mobile/login')
+    })
+  }
+
   return (
     <div>
       <h1>Signup Component</h1>
+      <input type="password" placeholder="비밀번호를 입력해주세요" />
+      <input type="password" placeholder="다시 한 번 입력해주세요" />
+
+      <button onClick={SignupClick}>회원가입</button>
     </div>
   )
 }
