52e16fa43ba0e81fa05a94934699d0f204b87b2e
JIEUN LEE <jin3409975@gmail.com>
2024-04-27 19:19:59 +0900

    S10P31B107-44-fe-feat: 마이페이지 로그아웃 완성

--- frontend/src/components/mobile/mypage/MyPage.tsx
+++ frontend/src/components/mobile/mypage/MyPage.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { startTransition, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const MyPage: React.FC = () => {
+  const navigate = useNavigate()
+
+  const LogoutClick = () => {
+    startTransition(() => {
+      // 로그아웃 전, accessToken의 존재 여부 확인
+      console.log('로그아웃 전 accessToken:', window.localStorage.getItem('accessToken'))
+
+      window.localStorage.removeItem('accessToken')
+
+      // 로그아웃 후, accessToken이 제거되었는지 확인
+      console.log('로그아웃 후 accessToken:', window.localStorage.getItem('accessToken'))
+
+      navigate('/mobile/start')
+    })
+  }
+
   return (
     <div>
+      <button onClick={LogoutClick}>로그아웃</button>
       <h1>MyPage Component</h1>
     </div>
   )
