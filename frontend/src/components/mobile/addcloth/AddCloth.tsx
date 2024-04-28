8d3691ec71a9baea3bceccc1ea386447cd118bd7
JIEUN LEE <jin3409975@gmail.com>
2024-04-28 20:44:14 +0900

    S10P31B107-51-fe-feat: AddCloth로  구글 검색 API 이동

--- frontend/src/components/mobile/addcloth/AddCloth.tsx
+++ frontend/src/components/mobile/addcloth/AddCloth.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { closetImgSearch, closetTextSearch } from '@/api/apiCloset'
+import Pagination from 'react-js-pagination'
 
 const AddCloth: React.FC = () => {
+  const [image, setImage] = useState(null)
+  const [text, setText] = useState('')
+  const [count, setCount] = useState(1)
+
+  async function textSearch() {
+    let response = await closetTextSearch(text)
+    setCount(response.data.result.length)
+    console.log(response.data.result)
+  }
+
+  function saveImage(event) {
+    setImage(event.target.files[0])
+  }
+
+  async function imageSearch() {
+    const formdata = new FormData()
+    formdata.append('image', image)
+    const response = await closetImgSearch(formdata)
+    console.log(response)
+    setCount(response.data.result.length)
+  }
+
+  function saveText(event) {
+    setText(event.target.value)
+  }
+
   return (
     <div>
       <h1>AddCloth Component</h1>
+      <input type="text" onChange={(e) => saveText(e)} placeholder="검색어를 입력 하세요..." />
+      <button onClick={() => textSearch()}>Text Search</button>
+      <input type="file" onChange={(e) => saveImage(e)}></input>
+      <button onClick={() => imageSearch()}>Image Search</button>
+      <Pagination totalItemsCount={count} onChange={(e) => console.log(e)}></Pagination>
     </div>
   )
 }
--- frontend/src/components/mobile/closet/ClosetSearch.tsx
+++ frontend/src/components/mobile/closet/ClosetSearch.tsx
@@ -1,32 +1,38 @@
 import React, { useState } from 'react'
 import { closetImgSearch, closetTextSearch } from '@/api/apiCloset'
 
+// 현재 코드는 구글 검색 코드입니다.
+// 이 화면에서는 각 프로필의 옷장 DB에 있는 옷을 검색할 수 있게 해야합니다.
+
 const ClosetSearch: React.FC = () => {
   const [image, setImage] = useState(null)
+  const [text, setText] = useState('')
 
-  async function textSearch(content: string) {
-    console.log(content)
-    let response = await closetTextSearch(content)
+  async function textSearch() {
+    let response = await closetTextSearch(text)
     console.log(response.data.result)
   }
 
   function saveImage(event) {
-    console.log(event)
     setImage(event.target.files[0])
   }
 
   async function imageSearch() {
-    console.log('123123')
     const formdata = new FormData()
     formdata.append('image', image)
     const response = await closetImgSearch(formdata)
     console.log(response)
   }
 
+  function saveText(event) {
+    setText(event.target.value)
+  }
+
   return (
     <div>
       <h1>ClosetSearch Component</h1>
-      <button onClick={() => textSearch('mantoman')}>Text Search</button>
+      <input type="text" onChange={(e) => saveText(e)} placeholder="검색어를 입력 하세요..." />
+      <button onClick={() => textSearch()}>Text Search</button>
       <input type="file" onChange={(e) => saveImage(e)}></input>
       <button onClick={() => imageSearch()}>Image Search</button>
     </div>
