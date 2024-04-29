1bbffcadff7f657eeae4e38315fdeae8906f34c6
JIEUN LEE <jin3409975@gmail.com>
2024-04-29 12:34:26 +0900

    S10P31B107-51-fe-feat: AddCloth 텍스트, 이미지 업로드 검색 결과 띄우기

--- frontend/src/components/mobile/addcloth/AddCloth.tsx
+++ frontend/src/components/mobile/addcloth/AddCloth.tsx
@@ -1,42 +1,80 @@
 import React, { useState } from 'react'
 import { closetImgSearch, closetTextSearch } from '@/api/apiCloset'
-import Pagination from 'react-js-pagination'
+// import Pagination from 'react-js-pagination'
+
+interface SearchResultItem {
+  price: string
+  link: string
+  image: string
+  title: string
+  source: string
+}
 
 const AddCloth: React.FC = () => {
   const [image, setImage] = useState(null)
   const [text, setText] = useState('')
-  const [count, setCount] = useState(1)
+  const [results, setResults] = useState<SearchResultItem[]>([])
+  // const [count, setCount] = useState(1)
 
-  async function textSearch() {
-    let response = await closetTextSearch(text)
-    setCount(response.data.result.length)
-    console.log(response.data.result)
+  // 가격이 있는 정보만 가져온다.
+  async function updateResults(data: any[]) {
+    const filteredResults = data.filter((item) => item.price !== null) as SearchResultItem[]
+    setResults(filteredResults)
   }
 
+  // 검색어 저장
+  function saveText(event) {
+    setText(event.target.value)
+  }
+
+  // 텍스트 검색
+  async function textSearch() {
+    let response = await closetTextSearch(text)
+    // setCount(response.data.result.length)
+    console.log(response.data.result)
+    updateResults(response.data.result)
+  }
+
+  // 업로드 이미지 저장
   function saveImage(event) {
     setImage(event.target.files[0])
   }
 
+  // 이미지 업로드 검색
   async function imageSearch() {
     const formdata = new FormData()
     formdata.append('image', image)
     const response = await closetImgSearch(formdata)
     console.log(response)
-    setCount(response.data.result.length)
+    // setCount(response.data.result.length)
+    updateResults(response.data.result)
   }
 
-  function saveText(event) {
-    setText(event.target.value)
+  // 결과 렌더링
+  function RenderResult() {
+    return (
+      <div>
+        {results.map((item: SearchResultItem, index: number) => (
+          <div key={index}>
+            <img src={item.image} alt={item.title} style={{ width: '100px', height: '100px' }} />
+            <p>{item.title}</p>
+            <p>{item.price}</p>
+            <p>{item.source}</p>
+          </div>
+        ))}
+      </div>
+    )
   }
 
   return (
     <div>
       <h1>AddCloth Component</h1>
-      <input type="text" onChange={(e) => saveText(e)} placeholder="검색어를 입력 하세요..." />
+      <input type="text" onChange={(e) => saveText(e)} placeholder="검색어를 입력하세요." />
       <button onClick={() => textSearch()}>Text Search</button>
       <input type="file" onChange={(e) => saveImage(e)}></input>
       <button onClick={() => imageSearch()}>Image Search</button>
-      <Pagination totalItemsCount={count} onChange={(e) => console.log(e)}></Pagination>
+      <RenderResult />
+      {/* <Pagination totalItemsCount={count} onChange={(e) => console.log(e)}></Pagination> */}
     </div>
   )
 }
--- frontend/src/components/mobile/closet/ClosetSearch.tsx
+++ frontend/src/components/mobile/closet/ClosetSearch.tsx
@@ -1,40 +1,40 @@
-import React, { useState } from 'react'
-import { closetImgSearch, closetTextSearch } from '@/api/apiCloset'
+import React from 'react'
+// import { closetImgSearch, closetTextSearch } from '@/api/apiCloset'
 
 // 현재 코드는 구글 검색 코드입니다.
 // 이 화면에서는 각 프로필의 옷장 DB에 있는 옷을 검색할 수 있게 해야합니다.
 
 const ClosetSearch: React.FC = () => {
-  const [image, setImage] = useState(null)
-  const [text, setText] = useState('')
+  //   const [image, setImage] = useState(null)
+  //   const [text, setText] = useState('')
 
-  async function textSearch() {
-    let response = await closetTextSearch(text)
-    console.log(response.data.result)
-  }
+  //   async function textSearch() {
+  //     let response = await closetTextSearch(text)
+  //     console.log(response.data.result)
+  //   }
 
-  function saveImage(event) {
-    setImage(event.target.files[0])
-  }
+  //   function saveImage(event) {
+  //     setImage(event.target.files[0])
+  //   }
 
-  async function imageSearch() {
-    const formdata = new FormData()
-    formdata.append('image', image)
-    const response = await closetImgSearch(formdata)
-    console.log(response)
-  }
+  //   async function imageSearch() {
+  //     const formdata = new FormData()
+  //     formdata.append('image', image)
+  //     const response = await closetImgSearch(formdata)
+  //     console.log(response)
+  //   }
 
-  function saveText(event) {
-    setText(event.target.value)
-  }
+  //   function saveText(event) {
+  //     setText(event.target.value)
+  //   }
 
   return (
     <div>
       <h1>ClosetSearch Component</h1>
-      <input type="text" onChange={(e) => saveText(e)} placeholder="검색어를 입력 하세요..." />
+      {/* <input type="text" onChange={(e) => saveText(e)} placeholder="검색어를 입력하세요." />
       <button onClick={() => textSearch()}>Text Search</button>
       <input type="file" onChange={(e) => saveImage(e)}></input>
-      <button onClick={() => imageSearch()}>Image Search</button>
+      <button onClick={() => imageSearch()}>Image Search</button> */}
     </div>
   )
 }
