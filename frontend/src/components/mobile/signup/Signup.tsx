5600f674263f3373bc9aa81b4ba9c03726818773
JoSangmin <139518200+JOSANGMIN777@users.noreply.github.com>
2024-04-30 12:43:41 +0900

    merge
    Former-commit-id: 98575485129a25141a608d595114d7afe2820d7e

--- frontend/src/components/mobile/signup/Signup.tsx
+++ frontend/src/components/mobile/signup/Signup.tsx
@@ -1,5 +1,5 @@
 import React, { startTransition, useState } from 'react'
-import { useNavigate, useLocation, Link } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 import { userCreate } from '@/api/apiUser'
 import styles from '../../../scss/signup.module.scss';
 
