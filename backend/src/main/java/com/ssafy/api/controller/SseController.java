2d12c0e47ba1eeb20f3e6e62186e1398ca452270
cyberjam <jamsince1994@naver.com>
2024-05-02 02:22:12 +0900

    S10P31B107-215-be-feat-init-sse-connection-and-generate-sessionId
    Former-commit-id: 3a0352b47231ad9778baa419d049f2d79bf6b5f4

---
+++ backend/src/main/java/com/ssafy/api/controller/SseController.java
@@ -0,0 +1,28 @@
+package com.ssafy.api.controller;
+
+import java.io.IOException;
+import java.util.UUID;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RestController
+@RequestMapping("/sse")
+public class SseController {
+    @GetMapping(value = "/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<SseEmitter> sseRequest() throws IOException {
+        final UUID sessionId = UUID.randomUUID();
+        final SseEmitter emitter = new SseEmitter(60 * 1000L);
+        try {
+            emitter.send(SseEmitter.event()
+                    .name("connect")
+                    .data(sessionId));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return ResponseEntity.ok(emitter);
+    }
+}
