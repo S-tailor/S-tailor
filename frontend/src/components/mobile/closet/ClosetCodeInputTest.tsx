eb516edc2937bdb2ab721749a8f494ff6d1ad80a
JIEUN LEE <jin3409975@gmail.com>
2024-05-03 22:11:26 +0900

    S10P31B107-2-fe-feat: 카메라 촬영 후 검색
    Former-commit-id: bd12ccd29382532517a297bfbbff68e7d0e20834

--- frontend/src/components/mobile/closet/ClosetCodeInputTest.tsx
+++ frontend/src/components/mobile/closet/ClosetCodeInputTest.tsx
@@ -5,48 +5,40 @@ import { QrReader } from 'react-qr-reader'
 
 const BASE_URL = 'https://ourtrip.store/api/tryon'
 // const BASE_URL = 'http://localhost:5000/api/tryon'
-const [camera, setCamera] = useState(true)
-
 
 const ClosetCodeInput: React.FC = () => {
-  
+  const [camera, setCamera] = useState(true)
   const navigate = useNavigate()
-  const [data, setData] = useState("");
+  const [data, setData] = useState('')
   const [message, setMessage] = useState('코드 인식이 완료되었습니다. 확인버튼을 눌러주세요.')
 
-
-  const handleVerify = async (event:React.SyntheticEvent) => {
-    event.preventDefault();
+  const handleVerify = async (event: React.SyntheticEvent) => {
+    event.preventDefault()
     if (data) {
-      const result = data.replace(/"/g, "");
-    
+      const result = data.replace(/"/g, '')
+
       const params = {
         profilePk: sessionStorage.getItem('profilePk'),
         id: localStorage.getItem('id'),
         token: localStorage.getItem('accessToken'),
         sessionId: result
       }
-     
+
       await axios
-      .post(`${BASE_URL}/verify`, params)
-      .then(function (response) {
-        if (response.data == 'success') {
-          setCamera(false)
-          navigate('/mobile/closet/tryon/wait')
-          
-         
-        }
-        
-      })
-      .catch(function (error) {
-        setMessage('다시 시도해주세요.')
-        console.log('error', error)
-      })
+        .post(`${BASE_URL}/verify`, params)
+        .then(function (response) {
+          if (response.data == 'success') {
+            setCamera(false)
+            navigate('/mobile/closet/tryon/wait')
+          }
+        })
+        .catch(function (error) {
+          setMessage('다시 시도해주세요.')
+          console.log('error', error)
+        })
     }
     setMessage('')
   }
-
-
 
   return (
     <div>
@@ -66,30 +58,25 @@ const ClosetCodeInput: React.FC = () => {
       <h1>QR코드 인증</h1>
       <p>기기에 표시된 QR코드를 촬영해주세요.</p>
       <div>
-        { camera &&
-      <QrReader
-        constraints={{ facingMode: 'environment' }}
-        onResult={(result: any | null, error) => {
-          if (result) {
-            setData(result.text);
-          }
-          if (error) {
-            console.info(error);
-          }
-        }}
-        />
-      }</div>
+        {camera && (
+          <QrReader
+            constraints={{ facingMode: 'environment' }}
+            onResult={(result: any | null, error) => {
+              if (result) {
+                setData(result.text)
+              }
+              if (error) {
+                console.info(error)
+              }
+            }}
+          />
+        )}
+      </div>
 
+      {data && <div>{message}</div>}
 
-
-      {data &&
-        <div>
-          
-          {message}
-        </div>}
-        
       <button onClick={handleVerify}>확인</button>
-        <form onSubmit={handleVerify}>
+      <form onSubmit={handleVerify}>
         <input
           type="text"
           value={data}
