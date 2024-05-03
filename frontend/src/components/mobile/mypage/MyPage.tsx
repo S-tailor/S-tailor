bf40ac34e738595df262fccf6c9ebaf6cb562af5
JIEUN LEE <jin3409975@gmail.com>
2024-05-03 13:26:22 +0900

    S10P31B107-242-fe-feat: 카메라 아이콘 전환
    Former-commit-id: 6b90be66a87a57424b0c7ce28263145bcee376e5

--- frontend/src/components/mobile/mypage/MyPage.tsx
+++ frontend/src/components/mobile/mypage/MyPage.tsx
@@ -1,15 +1,15 @@
-import React, { useState, useMemo, startTransition } from 'react'
+import React, { useMemo, startTransition } from 'react'
 import userStore from '@/store/store'
 import { useLocation, useNavigate } from 'react-router-dom'
-import styles from '../../../scss/mypage.module.scss';
+import styles from '../../../scss/mypage.module.scss'
 
 const MyPage: React.FC = () => {
   const navigate = useNavigate()
-  const location = useLocation();
+  const location = useLocation()
   const { user } = userStore() as {
-    user: { profilePk: number; image?: string; profileName: string }[];
-  };
-  const userName= user[0]?.profileName  ?? 'Guest'
+    user: { profilePk: number; image?: string; profileName: string }[]
+  }
+  // const userName = user[0]?.profileName ?? 'Guest'
 
   // 프로필 이름
 
@@ -49,47 +49,45 @@ const MyPage: React.FC = () => {
     const path = location.pathname
     const iconPaths: { [key: string]: { [icon: string]: string } } = {
       '/mobile/closet': {
-        'closet': '/src/assets/closetFill.png',
+        closet: '/src/assets/closetFill.png',
         'add-cloth': '/src/assets/upload.png',
-        'ask': '/src/assets/shirt.png',
-        'mypage': user[0]?.image || "/src/assets/avatar.PNG"
+        ask: '/src/assets/shirt.png',
+        mypage: user[0]?.image || '/src/assets/avatar.PNG'
       },
       '/mobile/add-cloth': {
-        'closet': '/src/assets/closet.png',
+        closet: '/src/assets/closet.png',
         'add-cloth': '/src/assets/uploadFill.png',
-        'ask': '/src/assets/shirt.png',
-        'mypage': user[0]?.image || "/src/assets/avatar.PNG"
+        ask: '/src/assets/shirt.png',
+        mypage: user[0]?.image || '/src/assets/avatar.PNG'
       },
       '/mobile/ask': {
-        'closet': '/src/assets/closet.png',
+        closet: '/src/assets/closet.png',
         'add-cloth': '/src/assets/upload.png',
-        'ask': '/src/assets/shirtFill.png',
-        'mypage': user[0]?.image || "/src/assets/avatar.PNG"
+        ask: '/src/assets/shirtFill.png',
+        mypage: user[0]?.image || '/src/assets/avatar.PNG'
       },
       '/mobile/mypage': {
-        'closet': '/src/assets/closet.png',
+        closet: '/src/assets/closet.png',
         'add-cloth': '/src/assets/upload.png',
-        'ask': '/src/assets/shirt.png',
-        'mypage': user[0]?.image || "/src/assets/avatar.PNG"
+        ask: '/src/assets/shirt.png',
+        mypage: user[0]?.image || '/src/assets/avatar.PNG'
       }
     }
-    return iconPaths[path][iconName] || '/src/assets/' + iconName + '.png';
+    return iconPaths[path][iconName] || '/src/assets/' + iconName + '.png'
   }
 
   const getMypageImgStyle = useMemo(() => {
-    return location.pathname === '/mobile/mypage' ? { border: '2px solid #9091FB', width: '9.5vw', height: '4.5vh', marginTop: '-2px'} : { filter: 'drop-shadow(0px 0px 1.5px #000000)' };
+    return location.pathname === '/mobile/mypage'
+      ? { border: '2px solid #9091FB', width: '9.5vw', height: '4.5vh', marginTop: '-2px' }
+      : { filter: 'drop-shadow(0px 0px 1.5px #000000)' }
   }, [location.pathname])
 
   const getActiveStyle = (path: string) => {
-    return location.pathname === path ? { fontFamily: 'Pretendard-Bold', color: '#9091FB', marginTop: '1px' } : {}
+    return location.pathname === path
+      ? { fontFamily: 'Pretendard-Bold', color: '#9091FB', marginTop: '1px' }
+      : {}
   }
   ////////////////////////////////////////////////////////////////////
-
-  const CameraClick = () => {
-    startTransition(() => {
-      navigate('/mobile/camera')
-    })
-  }
 
   return (
     <div>
@@ -109,62 +107,72 @@ const MyPage: React.FC = () => {
 
       <footer className={styles.bottomNav}>
         <div className={styles.bottomNavInner}>
-            <label className={styles.bottomNavInnerBtn}>
-              <img 
-                className={styles.closetImg}
-                src={getIconSrc('closet')}
-                alt="closet-home" 
-                onClick={()=>{
-                startTransition(()=>{
-                  navigate('/mobile/closet')})} 
-                }
-              />
-              <p className={styles.bottomNavLabel1} style={getActiveStyle('/mobile/closet')}>옷장 홈</p>
-            </label>
-           
-            <label className={styles.bottomNavInnerBtn}>
-              <img 
-                className={styles.addClothesImg}
-                src={getIconSrc('add-cloth')} 
-                alt="clothes-add" 
-                onClick={()=>{
-                startTransition(()=>{
-                  navigate('/mobile/add-cloth')})} 
-                }
-              />
-              <p className={styles.bottomNavLabel2} style={getActiveStyle('/mobile/add-cloth')}>옷 추가하기</p>
-            </label>
+          <label className={styles.bottomNavInnerBtn}>
+            <img
+              className={styles.closetImg}
+              src={getIconSrc('closet')}
+              alt="closet-home"
+              onClick={() => {
+                startTransition(() => {
+                  navigate('/mobile/closet')
+                })
+              }}
+            />
+            <p className={styles.bottomNavLabel1} style={getActiveStyle('/mobile/closet')}>
+              옷장 홈
+            </p>
+          </label>
 
-            <label className={styles.bottomNavInnerBtn}>
-              <img 
-                className={styles.recommendImg}
-                src={getIconSrc('ask')} 
-                alt="style-recomm" 
-                onClick={()=>{
-                startTransition(()=>{
-                  navigate('/mobile/ask')})} 
-                }
-              />
-              <p className={styles.bottomNavLabel3} style={getActiveStyle('/mobile/ask')}>스타일추천</p>
-            </label>
+          <label className={styles.bottomNavInnerBtn}>
+            <img
+              className={styles.addClothesImg}
+              src={getIconSrc('add-cloth')}
+              alt="clothes-add"
+              onClick={() => {
+                startTransition(() => {
+                  navigate('/mobile/add-cloth')
+                })
+              }}
+            />
+            <p className={styles.bottomNavLabel2} style={getActiveStyle('/mobile/add-cloth')}>
+              옷 추가하기
+            </p>
+          </label>
 
-            <label className={styles.bottomNavInnerBtn}>
-              <img 
-                className={styles.mypageImg}
-                src={getIconSrc('mypage')}
-                alt="myPage" 
-                style={getMypageImgStyle}
-                onClick={() => {
-                  startTransition(() => {
-                    navigate('/mobile/mypage')
-                  })
-                }}
-              />
-              <p className={styles.bottomNavLabel4} style={getActiveStyle('/mobile/mypage')}>마이페이지</p>
-            </label>
+          <label className={styles.bottomNavInnerBtn}>
+            <img
+              className={styles.recommendImg}
+              src={getIconSrc('ask')}
+              alt="style-recomm"
+              onClick={() => {
+                startTransition(() => {
+                  navigate('/mobile/ask')
+                })
+              }}
+            />
+            <p className={styles.bottomNavLabel3} style={getActiveStyle('/mobile/ask')}>
+              스타일추천
+            </p>
+          </label>
+
+          <label className={styles.bottomNavInnerBtn}>
+            <img
+              className={styles.mypageImg}
+              src={getIconSrc('mypage')}
+              alt="myPage"
+              style={getMypageImgStyle}
+              onClick={() => {
+                startTransition(() => {
+                  navigate('/mobile/mypage')
+                })
+              }}
+            />
+            <p className={styles.bottomNavLabel4} style={getActiveStyle('/mobile/mypage')}>
+              마이페이지
+            </p>
+          </label>
         </div>
       </footer>
-      
     </div>
   )
 }
