5b350d576a166a2527904018cf3da8ae9b276c41
Hana Na <gksk3185@gmail.com>
2024-05-09 10:35:47 +0900

    fix: image optimization + tryon code base URL edit
    Former-commit-id: ede2d55adcfce47d781ea462ca0ef315ae161382

--- frontend/src/store/store.tsx
+++ frontend/src/store/store.tsx
@@ -21,11 +21,9 @@ interface User {
   user: userProfile[]
   cartList: CartItem[]
   cartCount: number
-  // profilePk: number | 0
   setUser: (user: userProfile) => void
   clearUsers: () => void
   setCartCount: (count: number) => void
-  // setCartCounts: (list: CartItem[]) => void
   addToCart: (item: CartItem) => void
   removeFromCart: (pk: number) => void
   updateCartList: (list: CartItem[]) => void
@@ -37,11 +35,9 @@ const userStore = create(
       user: [],
       cartList: [],
       cartCount: 0,
-      // profilePk: 0,
       setUser: (by) => set(() => ({ user: [by] })),
       clearUsers: () => set({ user: [] }),
       setCartCount: (count) => set({ cartCount: count }),
-      // setCartCounts: (list) => set({ cartCount: list.length }),
       addToCart: (item) =>
         set((state) => {
           const newCartList = [...state.cartList, item]
