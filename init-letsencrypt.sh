b18eb7fc1b1e7f7bc482e0ff8b137357d7ffa2b5
root <root@ip-172-26-14-122.ap-northeast-2.compute.internal>
2024-05-10 00:25:33 +0000

    jenkins
    Former-commit-id: 284ac7646c14de578bf82a4441288f5075c38e3c

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(ourtrip.store)
+domains=(www.s-tailor.com)
 rsa_key_size=4096
 data_path="./certbot"
 email="jamsince1994@naver.com" # Adding a valid address is strongly recommended
