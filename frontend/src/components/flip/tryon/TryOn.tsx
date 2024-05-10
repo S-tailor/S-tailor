e5baea980f5a8f4452d780dceae528cfac6bfa89
JaeMin_Kim <jamsince1994@naver.com>
2024-05-10 15:52:41 +0900

    S10P31B107-112-fe-feat-try-on
    Former-commit-id: 29ef36de1b03cfe0d6d29cd04220540e16654ae8

--- frontend/src/components/flip/tryon/TryOn.tsx
+++ frontend/src/components/flip/tryon/TryOn.tsx
@@ -1,8 +1,13 @@
 import React, { useRef, useState, useEffect, useMemo } from 'react'
 import { CSSProperties } from 'react'
+import { tryOnGenerate } from '@/api/apiTryOn'
+
 const TryOn: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement>(null)
   const [isCameraOn, setIsCameraOn] = useState(false)
+  const [fileImage, setFileImage] = useState<File>()
+  const [fileUrl, setFileUrl] = useState<string>('')
+  const [resultUrl, setResultUrl] = useState<string>('')
 
   useEffect(() => {
     const initCamera = async () => {
@@ -39,11 +44,40 @@ const TryOn: React.FC = () => {
     []
   )
 
+  const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]
+    setFileImage(file)
+    if (file) {
+      setFileUrl(URL.createObjectURL(file))
+    }
+  }
+
+  const handleTryOnButton = async () => {
+    const formData = new FormData()
+    if (fileImage instanceof File) {
+      formData.append('model', fileImage)
+    }
+    formData.append(
+      'cloth',
+      'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQrZOJkf9outGmhW7vnfRic8qHvuJ-ZbpQ9ucUQJAeITpd5inhVtjN7_e-acDIFNS-tWUG2IcKEctJ27M2xELGtB2RuUwSrPw6wWEngHikdk_k8SmVDje45_g&usqp=CAE'
+    )
+    formData.append('profilePk', '12')
+    formData.append('category', 'lower_body')
+
+    const response = await tryOnGenerate(formData)
+    setResultUrl(response.data.generatedImageURL)
+  }
   return (
     <>
       <h1>TryOn Component</h1>
       <button onClick={toggleCamera}>{isCameraOn ? 'Turn Off Camera' : 'Turn On Camera'}</button>
+      <button onClick={handleTryOnButton}>Try On</button>
+      <div>{fileUrl && <img alt="originModel" src={fileUrl} />}</div>
+      <label htmlFor="imgFile">
+        <input type="file" name="imgFile" id="imgFile" onChange={handleFileChange} />
+      </label>
 
+      <div>{resultUrl && <img alt="result" src={resultUrl} />}</div>
       <video autoPlay ref={videoRef} style={videoStyle}></video>
     </>
   )
